10025f5183f065c4f20ba4f1210ac26cde5e1575
Antoine Glacet <antoine.glacet@kansai-airports.co.jp>
2021-06-18 23:53:16 +0900

    test

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='Simulation tools based on Simpy library applied to KAP terminal Pax flows',
-    author='Antoine Glacet',
-    license='GNU GPLv3',
+    version="0.1.0",
+    description="Simulation tools based on Simpy library applied to KAP terminal Pax flows",
+    author="Antoine Glacet",
+    license="GNU GPLv3",
 )
