6985d35e2574d33b4a85894834bb350c936f5b46
Antoine Glacet <glacet.antoine@gmail.com>
2022-07-15 17:51:26 +0900

    progress save

--- src/simfunc/KIX_T1d_CUSBD_new.py
+++ src/simfunc/KIX_T1d_CUSBD_new.py
@@ -1,13 +1,7 @@
-# KIX_T1d_CUSBD.py
-# includes:
-# - KIX_T1_CUSBD_departure_sim_function
-# - univariate_cost_function_generator_t1d_CUSBD
-# - cost_function_t1d_CUSBD_EBS
+# KIX_T1d_CUSBD_new.py
 
 import datetime
-import heapq
-import os
-import random
+from math import ceil
 
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
@@ -16,15 +10,18 @@ import pandas as pd
 import seaborn as sns
 import simpy
 from tqdm import tqdm
-from math import ceil
 
 from src.utils.profiles_from_schedule_new import SimParam
 
-FREQ = "5min"
-WINDOW = 1
+FREQ = "15min"
+WINDOW = 4
 
 
 def minutes_to_hms(minutes):
+    """
+    transforms a number of minutes to a str of hms on 2020-10-13
+    for datetime formatting
+    """
     if np.isnan(minutes):
         hms = np.nan
     else:
@@ -35,6 +32,47 @@ def minutes_to_hms(minutes):
             int((minutes % 1) * 60),
         )
     return hms
+
+
+class CustomResource(simpy.PriorityResource):
+    """
+    define a custom resource with extra functions
+    """
+
+    def __init__(self, env, startup_capacity: int):
+        super().__init__(env, 100)
+        self.max_capacity = 100
+        self.current_capacity = self.max_capacity
+        self.dummy_requests_list = []
+        self.set_capacity(startup_capacity)
+        self.current_capacity = startup_capacity
+
+        self.env = env
+
+    def set_capacity(self, target_capacity):
+        # use dummy priority 0 request to manage capacity
+        # we need to store the request to be able to release them later
+        diff_capa = self.current_capacity - target_capacity
+        for i in range(abs(diff_capa)):
+            if diff_capa > 0:
+                dummy_request = self.request(priority=0)
+                self.dummy_requests_list.append(dummy_request)
+            else:
+                self.release(self.dummy_requests_list[i])
+        self.current_capacity = target_capacity
+
+    def change_capa_per_schedule(self, serie_Counters_change):
+        previous_time = 0
+        for time, n_counters in serie_Counters_change.items():
+            if time == 0:
+                continue
+            yield self.env.timeout((time - previous_time) * 5)
+            # do I need to use self.env.process?
+            self.set_capacity(int(n_counters))
+            previous_time = time
+            # print("time = {} and n_counter = {}".format(time * 5, n_counters))
+            # print("current capa {}".format(self.current_capacity))
+            # print(self.dummy_requests_list)
 
 
 class Pax:
@@ -54,27 +92,77 @@ class Pax:
         self.std = row["Scheduled Time"]
         self.show_up_time = row["time"]
         # to put inside of simparam!
-        self.type = "CUSBD"
+        self.type = "tradi"
+
+    def go_to_process(self, process_str: str):
+        """
+        Pax queues and do process
+        Result stored to pax.row
+        """
+        with self.simulation.resources[process_str].request() as request:
+            self.row["{}_queue_length".format(process_str)] = len(
+                self.simulation.resources[process_str].queue
+            )
+            self.row["start_{}_queue".format(process_str)] = self.env.now
+            yield request
+            self.row["end_{}_queue".format(process_str)] = self.env.now
+            yield self.env.process(self.do_process(process_str))
+
+            self.row["end_{}_process".format(process_str)] = self.env.now
+
+    def go_to_checkin(self):
+        """
+        Pax queues and do checkin
+        Result stored to pax.row
+        """
+        process_str = "checkin"
+        with self.simulation.checkin[self.airline].request(priority=2) as request:
+            self.row["{}_queue_length".format(process_str)] = len(
+                self.simulation.checkin[self.airline].queue
+            )
+            self.row["start_{}_queue".format(process_str)] = self.env.now
+            yield request
+            self.row["end_{}_queue".format(process_str)] = self.env.now
+            yield self.env.process(self.do_process(process_str))
+
+            self.row["end_{}_process".format(process_str)] = self.env.now
+
+    def do_process(self, process_str: str):
+        """Pax does process, simple timeout"""
+        Pt = self.simulation.simparam.dct_processes[process_str] / 60
+        yield self.env.timeout(Pt)
+
+    def wait_opening(self, time_to_std: float):
+        """
+        wait for time_to_std before STD before going to next step
+        record results!
+        """
+        std = self.std.hour * 60 + self.std.minute + self.std.second / 60
+        t = std - time_to_std - self.env.now
+        if t > 0:
+            yield self.env.timeout(t)
 
     def depart(self):
         # advance sim time to pax show-up
         yield self.env.timeout(self.row["minutes"])
-        if self.type == "CUSBD":
-            # do kiosk process
-            with self.simulation.resources["kiosk"].request() as request:
-                self.row["kiosk_queue_length"] = len(
-                    self.simulation.resources["kiosk"].queue
-                )
-                self.row["start_kiosk_queue"] = self.env.now
-                yield request
-                self.row["end_kiosk_queue"] = self.env.now
-                yield self.env.process(self.checkin_2step_kiosk())
-                self.row["end_kiosk_process"] = self.env.now
 
-    def checkin_2step_kiosk(self):
-        """check-in at Kiosk"""
-        Pt_kiosk = self.simulation.simparam.dct_processes["kiosk"] / 60
-        yield self.env.timeout(Pt_kiosk)
+        if self.type == "CUSBD":
+            process_sequence = ["kiosk", "security"]
+
+            for process_str in process_sequence:
+                yield self.env.process(self.go_to_process(process_str))
+
+            yield self.env.process(self.wait_opening(2.5 * 60))
+            yield self.env.process(self.go_to_process("CUSBD"))
+
+        if self.type == "tradi":
+            process_sequence = ["kiosk"]
+
+            for process_str in process_sequence:
+                yield self.env.process(self.go_to_process(process_str))
+
+            yield self.env.process(self.wait_opening(2.5 * 60))
+            yield self.env.process(self.go_to_checkin())
 
 
 class Simulation:
@@ -91,19 +179,36 @@ class Simulation:
         self.env = env
         self.simparam = simparam
         self.df_Counters = simparam.df_Counters
+        self.dct_Counters_change = simparam.dct_Counters_change
 
-        # Create Simpy resources
+        # Create normal resources
         self.resources = {
-            "kiosk": simpy.PriorityResource(
-                self.env, self.simparam.dct_resource["kiosk"]
-            ),
+            process_str: simpy.PriorityResource(
+                self.env, self.simparam.dct_resource[process_str]
+            )
+            for process_str in self.simparam.dct_resource
         }
 
-        # Create Simpy resources specifically for check-in
+    def generate_checkin(self):
+        """
+        create the checkin resources and
+        make their capacity vary according to df_Counters info
+        we use dummy priority 0 (top) requests to block counters
+        """
+        # Create custon resources
         list_airlines = self.simparam.schedule["Airline Code"].unique()
         self.checkin = {
-            airline: simpy.PriorityResource(self.env, 1) for airline in list_airlines
+            airline: CustomResource(self.env, int(self.dct_Counters_change[airline][0]))
+            for airline in list_airlines
         }
+
+        # generate change of capacity events at the right time
+        for airline in self.checkin:
+            self.env.process(
+                self.checkin[airline].change_capa_per_schedule(
+                    self.dct_Counters_change[airline]
+                )
+            )
 
     def generate_pax(self):
         # generate a Pax for each pax of df_Pax
@@ -111,30 +216,35 @@ class Simulation:
         for pax in self.pax_list:
             self.env.process(pax.depart())
 
+    def run(self, end_time=1440):
+        # run simulation for a 24h
+        with tqdm(total=end_time - 1, desc="Simulation running...") as runpbar:
+            for i in range(1, end_time):
+                self.env.run(until=i)
+                runpbar.update(1)
+
     def format_df_result(self):
         # concatenate pax rows
         self.df_result = pd.concat(
             [pax.row for pax in self.pax_list], axis=1
         ).transpose()
 
-        self.df_result.rename(
-            columns={
-                "checkin_kiosk_queue_length": "kiosk_queue_length",
-                "start_checkin_kiosk_queue": "start_kiosk_queue",
-                "end_checkin_kiosk_queue": "end_kiosk_queue",
-                "end_checkin_kiosk_process": "end_kiosk_process",
-            },
-            inplace=True,
-        )
+        # sampling ratio
+        ratio_sampling = pd.to_timedelta("1H") / pd.to_timedelta(FREQ)
+
+        # list for iteration
+        list_process_all = ["kiosk", "checkin"]
 
         # different types of columns
-        datetime_columns = [
-            "start_kiosk_queue",
-            "end_kiosk_queue",
-            "end_kiosk_process",
+        datetime_columns_types = [
+            "start_{}_queue",
+            "end_{}_queue",
+            "end_{}_process",
         ]
-        # list for iteration
-        list_process_all = ["kiosk"]
+
+        datetime_columns = [
+            a.format(b) for b in list_process_all for a in datetime_columns_types
+        ]
 
         # change datetime columns to datetime
         for column in datetime_columns:
@@ -169,7 +279,6 @@ class Simulation:
         self.dct_plot = {
             key: [plot.format(key) for plot in list_plot] for key in list_process_all
         }
-        ratio_sampling = pd.to_timedelta("1H") / pd.to_timedelta(FREQ)
 
         # in
         self.plt_in = [
@@ -227,17 +336,8 @@ class Simulation:
             )
             for key in [*self.dct_plot]
         ]
+
         # histograms of queue duration and queue length
-
-        self.plt_hist_wait_time = [
-            (
-                self.df_result[self.df_result[self.dct_plot[key][0]].notnull()][
-                    self.dct_plot[key][3]
-                ].apply(lambda x: x.total_seconds() / 60)
-            )
-            for key in [*self.dct_plot]
-        ]
-
         self.dct_hist_wait_time = {
             key: (
                 self.df_result[self.df_result[self.dct_plot[key][0]].notnull()][
@@ -256,8 +356,16 @@ class Simulation:
             for key in [*self.dct_plot]
         }
 
+        self.plt_hist_wait_time = [value for value in self.dct_hist_wait_time.values()]
+
     def plot_result(self):
         n_graph = len([*self.dct_plot])
+        # we force n_graph>=2 for 2-dim axs (for indexing)
+        dummy_graph = False
+        n_graph_dummy = n_graph
+        if n_graph == 1:
+            n_graph_dummy = n_graph + 1
+            dummy_graph = True
 
         # plot param
         xmin = pd.to_datetime("2020-10-13 00:00:00")
@@ -271,10 +379,10 @@ class Simulation:
         widths = [4, 1]
         gs_kw = dict(width_ratios=widths)
 
-        fig = plt.figure(figsize=(16, 4 * n_graph))
+        fig = plt.figure(figsize=(16, 4 * n_graph_dummy))
 
-        axs = fig.subplots(n_graph, 2, squeeze=True, gridspec_kw=gs_kw)
-        ax2 = [axs[i, 0].twinx() for i in range(n_graph)]
+        axs = fig.subplots(n_graph_dummy, 2, squeeze=True, gridspec_kw=gs_kw)
+        ax2 = [axs[i, 0].twinx() for i in range(n_graph_dummy)]
 
         # plot for all processes, except 'wait for counter opening'
         for i in range(n_graph):
@@ -334,5 +442,11 @@ class Simulation:
         labels = axs[n_graph - 1, 0].get_xticklabels()
         plt.setp(labels, rotation=45, horizontalalignment="right")
         axs[n_graph - 1, 0].set(xlabel="time")
+
+        # remove dummy if needed
+        if dummy_graph == True:
+            axs[1, 0].remove()
+            axs[1, 1].remove()
+            ax2[1].remove()
 
         plt.show()
